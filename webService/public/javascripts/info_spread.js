170697853deca67d6547cc744b7e66cd335d1b2f
David Ding <dwding@merkleinc.com>
2019-08-14 17:50:47 +0800

    add text box

--- webService/public/javascripts/info_spread.js
+++ webService/public/javascripts/info_spread.js
@@ -1,4 +1,4 @@
-const height = window.innerHeight*0.7;
+const height = window.innerHeight * 0.7;
 const width = window.innerWidth;
 
 chart = function (data) {
@@ -15,7 +15,7 @@ chart = function (data) {
         .attr("viewBox", [0, 0, width, height]);
 
     const link = svg.append("g")
-        .attr("stroke", "#999")
+        .attr("stroke", "#333")
         .attr("stroke-opacity", 0.6)
         .selectAll("line")
         .data(links)
@@ -28,23 +28,49 @@ chart = function (data) {
         .selectAll("circle")
         .data(nodes)
         .join("circle")
-        .attr("r",15)
+        .attr("r", 15)
         .on("mousedown", function () {
-            var t = d3.transition().duration(500).ease(d3.easeBounceIn);
+            var t1 = d3.transition().duration(500).ease(d3.easeBounceIn);
+            var t2 = d3.transition().duration(700).ease(d3.easeQuadIn);
             d3.select(this).interrupt();
             if (d3.select(this).attr("r") == 15) {
-                d3.selectAll("circle").transition(t).attr("r", 15);
-                d3.select(this).transition(t).attr("r", 30);
+                d3.selectAll("circle").transition(t1).attr("r", 15);
+                d3.select(this).transition(t1).attr("r", 30);
+                d3.select(".tooltip-text").text(d3.select(this).text());
+                d3.select(".tooltip").style("display", "block");
+                d3.select(".tooltip").transition(t2).style("opacity", "1");
+                d3.select(".tooltip").style("z-index", 1000);
+                var radius = 15;
+                var cx = parseInt(d3.select(this).attr("cx"));
+                var cy = parseInt(d3.select(this).attr("cy"));
+                var x = cx + 2.5*radius;
+                var y = cy - radius;
+                d3.select(".tooltip").attr("x",x);
+                d3.select(".tooltip").attr("y",y);
             } else {
-                d3.select(this).transition(t).attr("r", 15);
+                d3.select(this).transition(t1).attr("r", 15);
+                d3.select(".tooltip").text("");
+                d3.select(".tooltip").transition(t2).style("opacity", "0");
+                d3.select(".tooltip").delay(700).style("display", "none");
             }
         })
         .select(function () {
             this.setAttribute("fill", "url(#" + data.nodes[$(this).index()].id + ")");
             return this;
         })
-        .call(drag(simulation))
+        .call(drag(simulation));
 
+    const tooltip = svg.append("g")
+        .append("rect")
+        .attr("class", "tooltip")
+        .attr("fill","#eaeaea")
+        .append("text")
+        .attr("class","tooltip-text")
+        .attr("x",50)
+        .attr("y",50)
+        .style("font-size",20)
+        .text("none")
+    
 
     const pattern = svg.append("defs")
         .selectAll("pattern")
@@ -61,6 +87,7 @@ chart = function (data) {
             image.setAttribute("xlink:href", "http://userimg.yingyonghui.com/head/24/1458708838143/5426424.png-thumb");
             this.innerHTML = image.outerHTML;
         });
+
     node.append("title")
         .text(d => d.nickName);
 
@@ -72,8 +99,8 @@ chart = function (data) {
             .attr("y2", d => d.target.y);
 
         node
-            .attr("cx", d =>  Math.max(30, Math.min(width - 30, d.x)))
-            .attr("cy", d =>  Math.max(30, Math.min(height - 30, d.y)));
+            .attr("cx", d => Math.max(30, Math.min(width - 30, d.x)))
+            .attr("cy", d => Math.max(30, Math.min(height - 30, d.y)));
     });
     return svg.node();
 }

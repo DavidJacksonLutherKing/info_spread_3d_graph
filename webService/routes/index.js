028bddf6a65e72cc37d0ec922453aeefcfd261d8
David Ding <dwding@merkleinc.com>
2019-09-17 17:53:15 +0800

    add new demo

--- webService/routes/index.js
+++ webService/routes/index.js
@@ -6,8 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Transmission' });
 });
 
-router.get('/demo', function(req, res, next) {
-  res.render('demo', { title: 'Demo' });
+router.get('/curve-force', function(req, res, next) {
+  res.render('curve-force', { title: 'Curve Force' });
 });
 /* GET Open Id 生成器. */
 router.get('/open_id_generator', function(req, res, next) {
